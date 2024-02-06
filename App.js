95651f6cf33ca973c79d8114e4ee80bc5617fb1c
vishprasad57 <159119165+vishprasad57@users.noreply.github.com>
2024-02-06 22:06:39 +0530

    my first push

---
+++ App.js
@@ -0,0 +1,30 @@
+
+/* const heading = React.createElement(
+    "h1",
+     {id : "heading"},
+      "Hello World from React!"
+    ); */
+
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    [React.createElement(
+        "div",
+        {id : "child"},
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")]
+    ),
+    React.createElement(
+        "div",
+        {id : "child2"},
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")]
+    )]
+)
+
+//JSX
+
+console.log(parent); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
